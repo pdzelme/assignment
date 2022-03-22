69de03a77d512e03c704ee89cbf73c545a0b70f0
Paula Paula <p@Paulas-MacBook-Air.local>
2022-03-22 18:37:13 +0200

    Homework

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.sql.SQLOutput;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// homework
+
+        int Price = 500;
+        double Discount = Price * 0.25;
+        System.out.println("Discount is 25% of Price:" + Discount) ;
+
+    }
+}
